df2f55cbccc2c71a38e21f1afa4fd6137550b56d
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-27 12:50:27 +0100

    Exercice 2 Done.

--- Phase_1_Algorithmie/exercice1.js
+++ Phase_1_Algorithmie/exercice1.js
@@ -1,1 +1,2 @@
+//1. Afficher "Hello, World!" dans la console.
 console.log("Hello, World!");
---
+++ Phase_1_Algorithmie/exercice2.js
@@ -0,0 +1,5 @@
+//2. Déclarer deux variables et afficher leur somme.
+let x,y;
+x = 5;
+y = 10;
+console.log(x+y)
