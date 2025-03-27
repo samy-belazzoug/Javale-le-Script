1946ccc9dbedd6d5ef2bf24079909bc4b2f97198
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-27 14:08:10 +0100

    Exercice 3 Done.

--- Phase_1_Algorithmie/Partie_1/exercice2.js
+++ Phase_1_Algorithmie/Partie_1/exercice2.js
@@ -1,5 +1,5 @@
 //2. Déclarer deux variables et afficher leur somme.
-let x,y;
+var x,y;
 x = 5;
 y = 10;
 console.log(x+y)
---
+++ Phase_1_Algorithmie/Partie_1/exercice3.js
@@ -0,0 +1,4 @@
+//3. Vérifier si un nombre est pair ou impair.
+const x = 8;
+if (x % 2 == 0) {console.log("Pair.");}
+else {console.log("Impair");}
