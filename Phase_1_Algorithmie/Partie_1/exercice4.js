8e55f7c9450e43bfecd56ea5e731ec49bd41d5f9
samy-belazzoug <samy.belazzoug@laplateforme.io>
2025-03-27 14:13:49 +0100

    Exercice 4 Done.

---
+++ Phase_1_Algorithmie/Partie_1/exercice4.js
@@ -0,0 +1,7 @@
+//4. Trouver le plus grand de trois nombres.
+const x = 1293, y = 0, z = 7; 
+var max = 0;
+if (max < x) {max = x;}
+if (max < y) {max = y;}
+if (max < z) {max = z;}
+console.log(max);
